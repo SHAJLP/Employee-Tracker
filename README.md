ee0912c9ae2d7edac69ab2ef2eac351bb5c034af
SHAJLP <sophiaaltaf73@gmail.com>
2023-02-07 19:38:05 +0000

    initial commit for seeds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Employee-Tracker
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg),[MIT](https://img.shields.io/badge/MIT-License-green)
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+<--departments-->
+
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Legal'),
+('Engineering'),
+('Finance');
+
+INSERT INTO role (title,salary, department-id)
+VALUES
+('Lead Software Engineer'),
+('Software Engineer'),
+('Sales Lead'),
+
+
